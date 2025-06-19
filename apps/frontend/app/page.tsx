648161175bafc3e694030e4de6bd7578ff67f585
hsnbd <hasanbd666@gmail.com>
2025-06-19 08:10:07 +0600

    add: feature overview

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -2,16 +2,19 @@ import Link from "next/link";
 import { RiSearch2Fill } from "react-icons/ri";
 import { FaShoppingCart } from "react-icons/fa";
 import { IoPerson } from "react-icons/io5";
+import { FaTruckFast } from "react-icons/fa6";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { IoArrowUndoCircleSharp } from "react-icons/io5";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <header className="flex h-[80vh] justify-between">
-        <div className="h-full w-full bg-[url('/images/HeroImage.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="h-full w-full bg-[url('/images/HeroImage.jpg')] bg-cover bg-center bg-no-repeat shadow-xl">
           {/* top header */}
           <div className="flex h-8 w-full items-center justify-center bg-gray-300">
             <h1 className="font-montserrat text-2xl text-gray-400">
-              top header part
+              Entregas a domicilio gratis
             </h1>
           </div>
 
@@ -74,8 +77,34 @@ export default function Home() {
           </div>
         </div>
       </header>
+
       {/* main */}
-      <main className="flex-1/3">main</main>
+      <main className="flex-1/3">
+        {/* feature overview */}
+        <div className="mt-10 flex h-full w-full items-center justify-center gap-10">
+          {/* fast delivery */}
+          <div className="flex h-full w-96 items-center justify-center bg-gray-200 px-4 py-3 shadow-xl">
+            <FaTruckFast size={30} />
+            <p className="font-montserrat pl-3 text-center text-xl">
+              Envío gratuito todo España
+            </p>
+          </div>
+          {/* call support */}
+          <div className="flex h-full w-96 items-center justify-center bg-gray-200 px-4 py-3 shadow-xl">
+            <FaPhoneSquareAlt size={30} />
+            <p className="font-montserrat pl-3 text-center text-xl">
+              Atención al cliente las 24hrs
+            </p>
+          </div>
+          {/* refund */}
+          <div className="flex h-full w-96 items-center justify-center bg-gray-200 px-4 py-3 shadow-xl">
+            <IoArrowUndoCircleSharp size={30} />
+            <p className="font-montserrat pl-3 text-center text-xl">
+              Garantía de devolución
+            </p>
+          </div>
+        </div>
+      </main>
       {/* footer */}
       <footer className="flex h-[10vh] justify-between">footer</footer>
     </div>
