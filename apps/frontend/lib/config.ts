136415b09d7e3e807a4d6713fc629d2d8b7b3947
hsnbd <hasanbd666@gmail.com>
2026-03-04 15:32:44 +0600

    init

--- apps/frontend/lib/config.ts
+++
@@ -1,5 +0,0 @@
-export const API_CONFIG = {
-  ORDER_SERVICE_URL: 'http://kube.ecom.local/services/orders',
-  USER_SERVICE_URL: 'http://kube.ecom.local/services/users',
-  BASE_URL: 'http://kube.ecom.local',
-};
